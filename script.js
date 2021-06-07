d2e765106a74225c813ba9ff0d28c1c80407d506
Faisaljabrah <>
2021-06-07 14:19:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let number = 6
+let number = 10
+let number = 5
+
+ocument.getElementById("caculate").addEventListener("click" caculate)
+
+function caculate () {
+number = document.getElementById("a").value
+number = document.getElementById("b").value
+number = document.getElementById("h").value
+
+number = parseInt(number)
+
+number = number (6+10/2x5)
+
+
+
+alert(number)
+}
